801513cd39bf0fd20dea9157006a8c66e745e5d4
hashbury1 <efefiomarchibong@gmail.com.com>
2025-12-14 09:55:21 +0100

    added test file

---
+++ app.test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+
+describe('Sample Test', () => {
+  it('should pass a basic test', () => {
+    assert.strictEqual(1 + 1, 2);
+  });
+  
+  it('should verify app logic', () => {
+    // Add your actual tests here
+    assert.ok(true);
+  });
+});
